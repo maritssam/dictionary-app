4da74aaa2f287aead1e38c2448d663c40e52d68b
JenLynX <104281812+JenLynX@users.noreply.github.com>
2022-09-28 14:17:18 -0700

    added search function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <h1>Dictionary App</h1>
+      <div className="container">
+        <h1>
+          <span className="book">📖</span> Dictionary App
+        </h1>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
     </div>
   );
 }
